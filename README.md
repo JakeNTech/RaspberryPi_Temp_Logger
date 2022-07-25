fa113bfbadfa91173da07a5ad7e5f6c2154fb483
JakeNTech <nenadicjake@gmail.com>
2022-07-25 12:03:55 +0100

    Adding latest for development

--- README.md
+++ README.md
@@ -2,48 +2,14 @@
 Some code to log and predict temperatures.
 
 ## Usage
-A `config.json` is used to store parameters about the script:
-```json
-{
-    "logging":{
-        "read_timeout":5, //Time between readings
-        "metric_units":true, //Celcius or Farenheight
-        "CSV_output_path":"./Output.csv" //Output location
-    },
-    "sensor_1":{
-        "name":"S1", // Sensor name in the output CSV file -> If you want to have each sensor in a different location
-        "wire_colour":"Green", //NOT USED -> So I can remember which sensor is which
-        "GPIO_pin":11, //NOT USED -> So I can remember which sensor is which
-        "D_pin":17 //NOT USED -> So I can remember which sensor is which
-    },
-    "sensor_2":{
-        "name":"S2", // Sensor name in the output CSV file -> If you want to have each sensor in a different location
-        "wire_colour":"Yellow", //NOT USED -> So I can remember which sensor is which
-        "GPIO_pin":13,  //NOT USED -> So I can remember which sensor is which
-        "D_pin":27  //NOT USED -> So I can remember which sensor is which
-    }
-}
-```
-Command line options can also be used, to load up a different config file <!-- and web server options-->:
-```console
-> python .\main.py -h
-Raspberry Pi Temperature & Humidity Logger!
-usage: main.py [-h] [-c <path>] [-w]
-
-A Python Temperature sensor logger and web server
-
-optional arguments:
-  -h, --help            show this help message and exit
-  -c <path>, --config <path>
-                        Location of the config file. Default: ./config.json
-  -w, --webserver       Run the Web server for a graphical output
-```
-## Notes
+yes
+## GPIO Pinout
 This is still very much under development and has yet to be tested with an actual sensor!\
 My GPIO Configuration, see [GPIO Pin out](https://www.raspberrypi-spy.co.uk/2012/06/simple-guide-to-the-rpi-gpio-header-and-pins/):
 
--   Sensor 1: GPIO Pin 11, D17, Green Wire
--   Sensor 2: GPIO Pin 13, D27, Yellow Wire
+-   Sensor 1: GPIO Pin 16, D23, Outdoor
+-   Sensor 2: GPIO Pin 18, D24, Indoor
+-   LCD Display: 
 
 ## To Do
 -   Test with actual sensor
--- main.py
+++ main.py
@@ -29,7 +29,7 @@ def load_config(config_file):
 
 def create_csv(output_file):
     csv_file = open(output_file,"w")
-    csv_file.write("Date/Time,Temperature("+config["sensor_1"]["name"]+"),Humidity("+config["sensor_1"]["name"]+")(%)\n")
+    csv_file.write("Date/Time,Temperature(Outdoor),Humidity(Outdoor)(%),Temperature(Indoor),Humidity(Indoor)(%)\n")
     csv_file.close()
 
 def log_to_csv(output_file,this_line):
@@ -38,8 +38,8 @@ def log_to_csv(output_file,this_line):
             writer.writerow(this_line)
 
 def read_log_temperature(timeout,metric_units):    
-    dhtSensor1 = adafruit_dht.DHT22(board.D27)
-    # dhtSensor2 = adafruit_dht.DHT22(board.D27)
+    dhtSensor1 = adafruit_dht.DHT22(board.D23)
+    dhtSensor2 = adafruit_dht.DHT22(board.D24)
 
     while True:
         #current_time = datetime.now().strftime("%Y-%m-%d %H%M:%S")
@@ -56,45 +56,21 @@ def read_log_temperature(timeout,metric_units):
             S1_humidity = None
             S1_temperature = None
         
-        # # Sensor 2
-        # try:
-        #     S2_humidity = dhtSensor2.humidity
-        #     S2_temperature = dhtSensor2.temperature
-        # except RuntimeError as error_msg:
-        #     error = f"{current_time},Run Time Error, Sensor 2"
-        #     error_logging(error)
-        #     S2_humidity = None
-        #     S2_temperature = None
+        # Sensor 2
+        try:
+            S2_humidity = dhtSensor2.humidity
+            S2_temperature = dhtSensor2.temperature
+        except RuntimeError:
+            S2_humidity = None
+            S2_temperature = None
 
         # TESTING NUMBERS
         # temperature = round(random.uniform(-40.0,80.0),1)
         # humidity = round(random.uniform(0.0,100.0),1)
 
-        if metric_units == False:
-            S1_temperature = S1_temperature * 9.0 / 5.0 + 32.0
-            # S2_temperature = S2_temperature * 9.0 / 5.0 + 32.0
-
-        # # If both sensors fail
-        # if S1_temperature == None and S2_temperature == None:
-        #     print("Both sensors reading failed...Skipping..")
-        #     error_logging(f"{current_time},Both Sensor reading failed,")
-        #     temperature_avg = None
-        #     humidity_avg = None
-        # # If sensor 1 failed -> Make Sensor 2 the Average
-        # elif S1_temperature == None and S1_humidity == None:
-        #     temperature_avg = S2_temperature
-        #     humidity_avg = S2_humidity
-        # # If sensor 2 failed -> Make Sensor 1 the Average
-        # elif S2_temperature == None and S2_humidity == None:
-        #     temperature_avg = S1_temperature
-        #     humidity_avg = S1_humidity
-        # # Otherwise Calculate the average between the two
-        # else:
-        #     temperature_avg = round((S1_temperature+S2_temperature)/2,2)
-        #     humidity_avg = round((S1_humidity+S2_humidity)/2,2)
 
         #"Date/Time,Temperature(S1),Temperature(S2),Humidity(S1)(%),Humidity(S2)(%),Average_Temp,Average_Humidity
-        log_to_csv(config["logging"]["CSV_output_path"],[current_time,S1_temperature,S1_humidity])
+        log_to_csv("./Output.csv",[current_time,S1_temperature,S1_humidity,S2_temperature,S2_humidity])
 
         time.sleep(timeout)
 
@@ -110,8 +86,10 @@ def lcd_display():
         # if the day varaible no longer is the current day
         if day != time.strftime("%a"):
             # clear max values
-            max_temp = 0
-            max_humid = 0
+            S1_max_temp = 0
+            S1_max_humid = 0
+            S2_max_temp = 0
+            S2_max_humid = 0
             # collect dawn/dusk times
             city = LocationInfo("London", "England", "Europe/London", 51.5, -0.116)
             s = sun(city.observer)
@@ -122,7 +100,11 @@ def lcd_display():
             # make the day variable = today
             day = time.strftime("%a")
 
-        gpio_display.lcd_display_string(time.strftime("%H:%M %a  %d/%m"),line=1)
+        s = f"{time.strftime('%H:%M')}     Indoor"
+        s2 = f"{time.strftime('%H:%M')}    Outdoor"
+        # Display Indoor Infomation
+        gpio_display.lcd_display_string(f"{time.strftime('%H:%M')}     Indoor",line=1)
+
         if S1_temperature == None or S1_humidity == None:
             gpio_display.lcd_display_string("Runtime Error :( ",line=2)
         else:            
@@ -130,19 +112,22 @@ def lcd_display():
         time.sleep(5)
 
         if isinstance(S1_temperature, float) and isinstance(S1_humidity, float):
-            if S1_temperature > max_temp:
-                max_temp = S1_temperature
-            if S1_humidity > max_humid and isinstance(S1_humidity, float):
-                max_humid = S1_humidity
+            if S1_temperature > S1_max_temp:
+                S1_max_temp = S1_temperature
+            if S1_humidity > S1_max_humid and isinstance(S1_humidity, float):
+                S1_max_humid = S1_humidity
+        if config["lcd_display"]["max_values"] == True:
+            gpio_display.lcd_display_string("Max Temp:  "+str(S1_max_temp)+"C",line=2)
+            time.sleep(5)
+            gpio_display.lcd_display_string("Max Humid: "+str(S1_max_humid)+"%",line=2)
+            time.sleep(5)
+        
 
-        gpio_display.lcd_display_string("Max Temp:  "+str(max_temp)+"C",line=2)
-        time.sleep(5)
-        gpio_display.lcd_display_string("Max Humid: "+str(max_humid)+"%",line=2)
-        time.sleep(5)
-        gpio_display.lcd_display_string("Dawn:      "+str(dawn),line=2)
-        time.sleep(2)
-        gpio_display.lcd_display_string("Dusk:      "+str(dusk),line=2)
-        time.sleep(2)
+        if config["lcd_display"]["dawn_dusk"] == True:
+            gpio_display.lcd_display_string("Dawn:      "+str(dawn),line=2)
+            time.sleep(2)
+            gpio_display.lcd_display_string("Dusk:      "+str(dusk),line=2)
+            time.sleep(2)
 
 if __name__ == "__main__":
     print("Raspberry Pi Temperature & Humidity Logger!")
@@ -158,13 +143,13 @@ if __name__ == "__main__":
     # if os.path.exists("Output.csv"):
     #     os.rename("./Output.csv",f"../Sample_Data/{datetime.now().strftime('%Y-%m-%dT%H%M:%S')}_Old.csv")
 
-    # create_csv(config["logging"]["CSV_output_path"])
+    create_csv(config["logging"]["CSV_output_path"])
 
     S1_temperature = 0
     S1_humidity = 0
 
     # Pass Temperature logging out to a new thread, can keep webserver running at same time! #multitasking
-    temp_log_thread = threading.Thread(target=read_log_temperature, args=(config["logging"]["read_timeout"],config["logging"]["metric_units"]))
+    temp_log_thread = threading.Thread(target=read_log_temperature, args=(5))
     display_thread = threading.Thread(target=lcd_display)
     temp_log_thread.start()
     display_thread.start()
